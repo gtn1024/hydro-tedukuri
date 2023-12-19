ca8e0d44236e1db5abc346c859f7304647d5bfd4
Taoning Ge <gtn1024o@outlook.com>
2023-12-19 16:57:39 +0800

    更新脚本

--- new.js
+++ new.js
@@ -2,23 +2,26 @@ const fs = require('fs');
 const path = require('path');
 
 // count current dirs
-const dirs = fs.readdirSync(__dirname);
-const count = dirs.filter(dir => dir.match(/^(\d+)$/)).length;
-fs.mkdirSync(path.join(__dirname, `${count + 1}`));
-fs.mkdirSync(path.join(__dirname, `${count + 1}`, 'additional_file'));
-fs.mkdirSync(path.join(__dirname, `${count + 1}`, 'testdata'));
+const dirs = fs.readdirSync(__dirname).filter(dir => dir.match(/^(\d+)$/));
+let newFolder = 0;
+for (const dir of dirs) {
+  newFolder = Math.max(newFolder, +dir + 1)
+}
+fs.mkdirSync(path.join(__dirname, `${newFolder}`));
+fs.mkdirSync(path.join(__dirname, `${newFolder}`, 'additional_file'));
+fs.mkdirSync(path.join(__dirname, `${newFolder}`, 'testdata'));
 const testdataConfig = `type: default
 time: 1000ms
 memory: 256MB
 `
-fs.writeFileSync(path.join(__dirname, `${count + 1}`, 'testdata', 'config.yaml'), testdataConfig);
+fs.writeFileSync(path.join(__dirname, `${newFolder}`, 'testdata', 'config.yaml'), testdataConfig);
 
-const problemConfig = `title: P${count + 1}
+const problemConfig = `title: P${newFolder}
 tag:
 - 算法竞赛进阶指南
-pid: P${count + 1}
+pid: P${newFolder}
 `
-fs.writeFileSync(path.join(__dirname, `${count + 1}`, 'problem.yaml'), problemConfig);
+fs.writeFileSync(path.join(__dirname, `${newFolder}`, 'problem.yaml'), problemConfig);
 
 const statement = `## 题目描述
 
@@ -47,4 +50,4 @@ const statement = `## 题目描述
 \`\`\`
 
 `
-fs.writeFileSync(path.join(__dirname, `${count + 1}`, 'problem_zh.md'), statement);
+fs.writeFileSync(path.join(__dirname, `${newFolder}`, 'problem_zh.md'), statement);
